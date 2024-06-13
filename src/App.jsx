1f91e59c81311606652de3290bca465d6a68dbfb
sahil khatri <sahilkeshav1997@gmail.com>
2024-06-13 09:41:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import Title from './Title'
+import menu from './data'
+import Menu from './Menu'
+import Categories from './Categories'
+
+const allCategories = ['all', ...new Set(menu.map((item) => item.category))]
+
+const App = () => {
+  const [menuItems, setMenuItems] = useState(menu)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(menu)
+      return
+    }
+
+    const newCategories = menu.filter((item) => item.category === category)
+
+    setMenuItems(newCategories)
+  }
+
+  return (
+    <main>
+      <section className='menu'>
+        <Title text='our menu' />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  )
+}
+export default App
